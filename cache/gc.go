7d6d131d915d9068393d57ddb177f986630eec38
Kunal Kushwaha <kushwaha_kunal_v7@lab.ntt.co.jp>
2017-11-28 09:54:48 +0900

    WIP : basic cache prune

--- cache/gc.go
+++ cache/gc.go
@@ -29,44 +29,28 @@ func (cm *cacheManager) Prune(ctx context.Context) (map[string]int64, error) {
 	//	return nil, nil
 	for _, cr := range cm.records {
 		fmt.Println("analyising container id : ", cr.ID())
-		//	cr.mu.Lock()
-		// ignore duplicates that share data
-		/*if cr.equalImmutable != nil && len(cr.equalImmutable.refs) > 0 || cr.equalMutable != nil && len(cr.refs) == 0 {
-			cr.mu.Unlock()
-			fmt.Println("Lets skip, id : ", cr.ID())
-			continue
-		}*/
-		fmt.Println("Determining Size ", cr.ID())
-		size, err := cr.Size(ctx)
-		if err != nil {
-			//	cr.mu.Unlock()
-			fmt.Println("Cannot determine size ", cr.ID())
-			return nil, err
-		}
-		if size >= 0 {
-			fmt.Println("Removing.. : ", cr.ID())
-			if cr.Parent() != nil {
-				fmt.Println("Tree Below")
-			}
-			for temp := cr.Parent(); temp == nil; temp = temp.Parent() {
-				fmt.Println("-> ", temp.ID())
-				//	temp = temp.Parent()
 
-			}
-			cr.Parent().ID()
-			/*err := cr.remove(ctx, true)
+		fmt.Println("Determining Size ", cr.ID())
+		/*	size, err := cr.Size(ctx)
 			if err != nil {
 				//	cr.mu.Unlock()
-				fmt.Println("Cannot delete the cache ", cr.ID(), err)
-				return nil, err
+				fmt.Println("Cannot determine size ", cr.ID())
+				continue
+				//	return nil, err
 			}*/
-			fmt.Println("Success..  ")
+		err := cr.mref().release(ctx)
+		if err != nil {
+			//	cr.mu.Unlock()
+			fmt.Println("Cannot delete the cache ", cr.ID(), err)
+			//return nil, err
+			continue
 		}
-		//	cr.mu.Unlock()
-		//		fmt.Println("Size determined : ", size)
+		fmt.Println("Success..  ")
+
 	}
 	fmt.Println("Returning")
-	return nil, nil
+	removeMap := make(map[string]int64)
+	return removeMap, nil
 }
 
 func (cm *cacheManager) GC(ctx context.Context) error {
